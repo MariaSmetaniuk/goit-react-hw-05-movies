0c0f63496d20797c3f9afa3c5ee5a510cd473010
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-31 09:37:36 +0200

    base routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
-// import { Box } from './Box';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { HomePage } from 'pages/HomePage/HomePage';
+import { MoviesPage } from 'pages/MoviesPage/MoviesPage';
+import { MovieDetailsPage } from 'pages/MovieDetailsPage/MovieDetailsPage';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
 
 export const App = () => {
   return (
     <>
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        React homework template
-      </div>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
+      </Routes>
 
       <GlobalStyle />
     </>
