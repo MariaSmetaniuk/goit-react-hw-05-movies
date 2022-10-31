6351ab48421ef285aa669f02a47f08367ba4f7ae
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-31 09:36:27 +0200

    getTrending api

---
+++ src/services/api.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '84c663b0d9ed75aa92613d5f25b42077';
+
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: API_KEY,
+  },
+});
+
+export const getTrendingMovies = async () => {
+  const { data } = await instance.get('trending/movie/day');
+  return data;
+};
